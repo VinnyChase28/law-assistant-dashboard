7ece6fbc119ff8eb6216c48c0e2ce0a601ed2bcb
VinnyChase28 <vincentgauthier068@gmail.com>
2023-11-13 00:08:11 -0800

    restore api calls

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { getServerAuthSession } from "src/server/auth";
 import { api } from "src/trpc/server";
 
 export default async function Home() {
+  const hello = await api.post.hello.query({ text: "from tRPC" });
   const session = await getServerAuthSession();
 
   return (
@@ -38,6 +39,10 @@ export default async function Home() {
           </Link>
         </div>
         <div className="flex flex-col items-center gap-2">
+          <p className="text-2xl text-white">
+            {hello ? hello.greeting : "Loading tRPC query..."}
+          </p>
+
           <div className="flex flex-col items-center justify-center gap-4">
             <p className="text-center text-2xl text-white">
               {session && <span>Logged in as {session.user?.name}</span>}
@@ -61,7 +66,7 @@ async function CrudShowcase() {
   const session = await getServerAuthSession();
   if (!session?.user) return null;
 
-  const latestPost = await api?.post?.getLatest?.query();
+  const latestPost = await api.post.getLatest.query();
 
   return (
     <div className="w-full max-w-xs">
