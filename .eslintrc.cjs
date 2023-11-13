f366bae78aecafd00eb11f6e67b6ac134d88cc5b
VinnyChase28 <vincentgauthier068@gmail.com>
2023-11-13 04:06:26 -0800

    more rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,6 +18,8 @@ const config = {
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/consistent-indexed-object-style": "off",
     "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-unsafe-return": "off",
     "@typescript-eslint/non-nullable-type-assertion-style": "off",
     "@typescript-eslint/ban-types": "off",
     "@typescript-eslint/no-floating-promises": "off",
