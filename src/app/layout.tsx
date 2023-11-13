2fc53efc742e3395be8361c16b5661162317657c
VinnyChase28 <vincentgauthier068@gmail.com>
2023-11-12 23:31:50 -0800

    add all

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import "src/styles/globals.css";
+
+import { Inter as FontSans } from "next/font/google";
+import { cookies } from "next/headers";
+import { cn } from "@/lib/utils";
+
+import { TRPCReactProvider } from "src/trpc/react";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable,
+        )}
+      >
+        <TRPCReactProvider cookies={cookies().toString()}>
+          {children}
+        </TRPCReactProvider>
+      </body>
+    </html>
+  );
+}
