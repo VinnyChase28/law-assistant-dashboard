61216aad2563ca1ed3bbbb00fd17762aea55e52a
Vince <vince.gauthier@replicant.ai>
2023-11-15 00:19:15 -0800

    better typescript

--- src/app/_components/drag-drop/file-manager.tsx
+++ src/app/_components/drag-drop/file-manager.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-
 import React, { useState } from "react";
 import { DndContext } from "@dnd-kit/core";
 
@@ -9,81 +8,85 @@ import { Draggable } from "./draggable";
 import { Label } from "@/components/ui/label";
 import { InputFile } from "../input-file";
 import { truncateFileName } from "src/app/helpers/textTransformers";
-import PopOverButton from "../popover";
 
-type FileKey = "Legal" | "Projects" | "New";
-const fileKeys: FileKey[] = ["Legal", "Projects"];
+type FileKey = string; // Since folder names are dynamic, we use string
+type File = { name: string /* other file properties */ };
+type FilesState = Record<FileKey, File[]>;
+type FoldersState = Record<FileKey, File[]>;
 
 export default function FileManager() {
-  const containers = ["New", "Legal", "Projects"];
-
-  const [files, setFiles] = useState<Record<FileKey, any[]>>({
+  const [files, setFiles] = useState<FilesState>({
     Legal: [],
     Projects: [],
     New: [],
   });
 
-  const [folders, setFolders] = useState<Record<string, any[]>>({
+  const [folders, setFolders] = useState<FoldersState>({
     Legal: [],
     Projects: [],
   });
 
-  const handleFilesChange = (newFiles: any) => {
+  const handleFilesChange = (newFiles: FileList) => {
     setFiles({
       ...files,
-      New: [...files.New, ...Array.from(newFiles)],
+      New: [
+        ...(files.New ?? []),
+        ...Array.from(newFiles).map((file) => ({ name: file.name })),
+      ],
     });
   };
 
   const handleDragEnd = (event: any) => {
     const { active, over } = event;
 
-    if (over) {
-      const sourceContainer = active.id.split("-")[0] as FileKey;
-      const targetContainer = over.id as FileKey;
+    if (over && active.id !== over.id) {
+      const sourceContainer = active.id.split("-")[0];
+      const targetContainer = over.id;
       const fileIndex = parseInt(active.id.split("-")[2], 10);
 
-      // Check if the source and target containers are the same
-      if (sourceContainer === targetContainer) {
-        // If they are the same, do nothing to avoid duplication
-        return;
-      }
+      // Check if sourceContainer and targetContainer are valid keys in files
+      if (sourceContainer in files && targetContainer in files) {
+        const sourceFiles = files[sourceContainer];
+        const targetFiles = files[targetContainer] ?? []; // Fallback to empty array if undefined
 
-      if (files[sourceContainer] && files[targetContainer]) {
-        const file = files[sourceContainer][fileIndex];
+        // Ensure sourceFiles is defined before accessing it
+        if (sourceFiles) {
+          const file = sourceFiles[fileIndex];
 
-        if (file) {
-          setFiles({
-            ...files,
-            [sourceContainer]: files[sourceContainer].filter(
-              (_, index) => index !== fileIndex,
-            ),
-            [targetContainer]: [...files[targetContainer], file],
-          });
+          if (file) {
+            setFiles({
+              ...files,
+              [sourceContainer]: sourceFiles.filter(
+                (_, index) => index !== fileIndex,
+              ),
+              [targetContainer]: [...targetFiles, file],
+            });
+          }
         }
       }
     }
   };
 
-  const addNewFolder = (folderName: string) => {
-    setFolders({
-      ...folders,
-      [folderName]: [],
-    });
+  const addNewFolder = () => {
+    const folderName = prompt("Enter folder name");
+    if (folderName && !folders[folderName]) {
+      setFolders({
+        ...folders,
+        [folderName]: [],
+      });
 
-    setFiles({
-      ...files,
-      [folderName]: [],
-    });
+      setFiles({
+        ...files,
+        [folderName]: [],
+      });
+    }
   };
 
   return (
     <div className="justify-center">
       <div className="flex justify-center">
         <InputFile onFilesChange={handleFilesChange} />
-        <div>
-          <PopOverButton />
-        </div>
+        <button onClick={addNewFolder}>Add Folder</button>
       </div>
       <DndContext onDragEnd={handleDragEnd}>
         <div className="m-3 flex flex-wrap items-start justify-center text-center">
@@ -91,34 +94,29 @@ export default function FileManager() {
           <div className="mb-4 w-full px-2">
             <Droppable
               id="New"
-              className="border-gray-25 min-h-[100px] w-full min-w-[300px] border"
+              className="border-gray-25 min-h-[200px] w-full min-w-[900px] border"
             >
-              {files.New.map((file, index) => (
+              {files.New?.map((file, index) => (
                 <Draggable key={index} id={`New-file-${index}`}>
                   {truncateFileName(file.name)}
                 </Draggable>
-              ))}
+              )) ?? []}
             </Droppable>
           </div>
 
-          {/* Legal and Projects Sections */}
-          <div className="flex w-full flex-wrap">
-            {fileKeys.map((id: FileKey) => (
-              <div key={id} className="mb-4 w-full px-2 md:w-1/2">
-                <Label>{id} Docs</Label>
-                <Droppable
-                  id={id}
-                  className="min-h-[300px] w-full min-w-[300px] border border"
-                >
-                  {files[id].map((file, index) => (
-                    <Draggable key={index} id={`${id}-file-${index}`}>
-                      {truncateFileName(file.name)}
-                    </Draggable>
-                  ))}
-                </Droppable>
-              </div>
-            ))}
-          </div>
+          {/* Dynamic Folders */}
+          {Object.keys(folders).map((id) => (
+            <div key={id} className="mb-4 w-full px-2 md:w-1/2">
+              <Label>{id} Docs</Label>
+              <Droppable id={id} className="min-h-[200px] min-w-[200px] border">
+                {(files[id] || []).map((file, index) => (
+                  <Draggable key={index} id={`${id}-file-${index}`}>
+                    {truncateFileName(file.name)}
+                  </Draggable>
+                ))}
+              </Droppable>
+            </div>
+          ))}
         </div>
       </DndContext>
     </div>
