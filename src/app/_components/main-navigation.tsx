7ef0ea649cb6fadb1a391b01c5c87cb81749e00d
VinnyChase28 <vincentgauthier068@gmail.com>
2023-11-18 04:09:00 -0800

    remove dead pages

--- src/app/_components/main-navigation.tsx
+++ src/app/_components/main-navigation.tsx
@@ -92,12 +92,9 @@ export function MainNavigation() {
                     className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
                     href="/"
                   >
-                    <div className="mb-2 mt-4 text-lg font-medium">
-                      shadcn/ui
-                    </div>
+                    <div className="mb-2 mt-4 text-lg font-medium">CaseyAI</div>
                     <p className="text-sm leading-tight text-muted-foreground">
-                      Beautifully designed components built with Radix UI and
-                      Tailwind CSS.
+                      Your very own AI legal assistant.
                     </p>
                   </a>
                 </NavigationMenuLink>
