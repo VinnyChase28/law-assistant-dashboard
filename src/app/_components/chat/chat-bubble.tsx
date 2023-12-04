d330318287439ea904f9d12d11a3f48c9b9f2b25
Vince <vincentgauthier068@gmail.com>
2023-12-04 00:41:10 -0800

    add options to chat

--- src/app/_components/chat/chat-bubble.tsx
+++ src/app/_components/chat/chat-bubble.tsx
@@ -1,5 +1,7 @@
 "use client";
-import React from "react";
+
+import React, { useEffect, useRef } from "react";
+import Typed from "typed.js";
 import { ChatBubbleIcon } from "@radix-ui/react-icons";
 import {
   Dialog,
@@ -12,6 +14,20 @@ import {
 import { Input } from "../ui/input";
 
 const ChatBubble = () => {
+  const el = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ["Hello! What would you like to do?"],
+      typeSpeed: 50,
+    });
+
+    return () => {
+      // Destroy Typed instance during cleanup to stop animation
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -24,7 +40,20 @@ const ChatBubble = () => {
           <DialogTitle>Chat</DialogTitle>
           <DialogDescription>Enter your message below</DialogDescription>
         </DialogHeader>
-        <div className="overflow-y-auto">this is for messages</div>
+        <div className="overflow-y-auto">
+          <span ref={el} />
+        </div>
+        <div className="my-4 flex justify-around">
+          <button className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
+            General chat
+          </button>
+          <button className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
+            Chat with docs
+          </button>
+          <button className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
+            Create report
+          </button>
+        </div>
         <Input
           type="text"
           className="mt-2 rounded-md border border-gray-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
