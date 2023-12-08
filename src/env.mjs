606f496647f58dbbfb70b00bded18d36f6394402
VinnyChase28 <vincentgauthier068@gmail.com>
2023-12-08 14:37:14 -0800

    ensure correct var

--- src/env.mjs
+++ src/env.mjs
@@ -53,7 +53,7 @@ export const env = createEnv({
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
     AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
-    AUTH0_ISSUER_BASE_URL: process.env.AUTH0_ISSUER,
+    AUTH0_ISSUER_BASE_URL: process.env.AUTH0_ISSUER_BASE_URL,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
