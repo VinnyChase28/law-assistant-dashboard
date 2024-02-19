98932c25f1c14eb40cf2423dfa618f5343f56c5f
VinnyChase28 <vincentgauthier068@gmail.com>
2024-02-18 23:35:28 -0800

    change naming

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,9 +1,10 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
-import helloWorld from "src/inngest/functions/compliance-reports";
+import complianceReports from "src/inngest/functions/compliance-reports";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [helloWorld],
+  functions: [complianceReports],
   streaming: "allow",
 });
+
