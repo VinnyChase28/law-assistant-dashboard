02584bdd23a90183e0ab65fc293074c4cf9b0514
Vince <vincentgauthier068@gmail.com>
2024-02-23 12:49:12 -0800

    remove unecessary class

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,18 +1,13 @@
 import OpenAI from "openai";
 import { OpenAIStream, StreamingTextResponse } from "ai";
+import { openai } from "src/utils/openai";
 
-// Create an OpenAI API client (that's edge friendly!)
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY ?? "",
-});
-
-// IMPORTANT! Set the runtime to edge
 export const runtime = "edge";
 
 export async function POST(req: Request) {
   const { messages } = await req.json();
 
-   console.log(messages, "messages");
+  console.log(messages, "messages");
 
   // Ask OpenAI for a streaming chat completion given the prompt
   const response = await openai.chat.completions.create({
