db40d101334fd60957671f9a5443d8ad81851dbf
VinnyChase28 <vincentgauthier068@gmail.com>
2024-03-05 17:35:59 -0800

    remove interval

--- src/app/settings/billing/page.tsx
+++ src/app/settings/billing/page.tsx
@@ -19,7 +19,6 @@ export default function SettingsAccountPage() {
       refetchOnReconnect: true,
       refetchOnMount: true,
       refetchOnWindowFocus: true,
-      refetchInterval: 1000,
     });
 
   if (isLoading) {
