ef5f275362075ea9abd61690032132e69e468736
VinnyChase28 <vincentgauthier068@gmail.com>
2024-03-07 11:39:44 -0800

    proper spacing

--- src/app/dashboard/settings/profile-form.tsx
+++ src/app/dashboard/settings/profile-form.tsx
@@ -187,34 +187,42 @@ export function ProfileForm() {
           />
           <div className="pt-6">
             <FormLabel className="pb-5">URLs</FormLabel>
+            <FormDescription>
+              Add your social or business links.
+            </FormDescription>
+            <br />
             {fields.map((field, index) => (
-              <div
-                key={field.id}
-                className="flex items-center space-x-2"
-                style={{ width: "100%" }}
-              >
-                <FormField
-                  control={form.control}
+              <div>
+                <br />
+                <div
                   key={field.id}
-                  name={`urls.${index}.value`}
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormControl>
-                        <Input {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <button
-                  type="button"
-                  onClick={() => remove(index)}
-                  className="text-gray-500 hover:text-gray-700"
+                  className="flex items-center space-x-2"
+                  style={{ width: "100%" }}
                 >
-                  <Trash2 size={20} />
-                </button>
+                  <FormField
+                    control={form.control}
+                    key={field.id}
+                    name={`urls.${index}.value`}
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormControl>
+                          <Input {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <button
+                    type="button"
+                    onClick={() => remove(index)}
+                    className="text-gray-500 hover:text-gray-700"
+                  >
+                    <Trash2 size={20} />
+                  </button>
+                </div>
               </div>
             ))}
+
             <br />
             <Button
               type="button"
