ad3cb9b9f559dea475bbef90c4dc191a08af1a5a
VinnyChase28 <vincentgauthier068@gmail.com>
2024-03-07 23:01:17 -0800

    remove uneeded code

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,7 @@
 import Charts from "@/components/charts/charts";
 import { Text } from "@tremor/react";
-import { redirect } from "next/navigation";
-import { getServerAuthSession } from "src/server/auth";
 
 const DashboardPage = () => {
-  const session = getServerAuthSession();
-  if (!session) {
-    redirect("/auth/sign-in");
-  }
   return (
     <>
       <Text className="ml-10 pt-5 text-3xl font-bold">Dashboard</Text>
