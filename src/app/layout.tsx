3284394ea7d1cbc5c6c988805f8f2cf013cff5f9
VinnyChase28 <vincentgauthier068@gmail.com>
2024-03-07 16:39:15 -0800

    hide website route for now

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,9 +45,9 @@ export default async function RootLayout({
           >
             <TooltipProvider>
               {/* Header container */}
-              <header className="flex w-full items-center justify-between px-4 py-2 shadow-md">
-                {/* add a header for non authed pages*/}
-              </header>
+              {/* <header className="flex w-full items-center justify-between px-4 py-2 shadow-md">
+
+              </header> */}
               {/* Main content */}
               <main className="flex w-full flex-1 flex-col">{children}</main>
               {/* Toast notifications */}
