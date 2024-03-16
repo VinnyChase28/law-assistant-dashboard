5457879abb1f3018f447d8a6eceee6ee719f0ef6
VinnyChase28 <vincentgauthier068@gmail.com>
2024-03-15 23:15:59 -0700

    remove bad marketing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,31 +3,8 @@ export const metadata = {
   description: "Landing page for Law Assistant AI",
 };
 
-import { Brands } from "./_components/marketing/Brands";
-import { Hero } from "./_components/marketing/Hero";
-import { Features1 } from "./_components/marketing/Features1";
-import { CaseStudies } from "./_components/marketing/CaseStudies";
-import { Divider } from "./_components/marketing/Divider";
-import { Testimonials } from "./_components/marketing/Testimonials";
-import { FeaturesDiagonal } from "./_components/marketing/FeaturesDiagonal";
-import { Pricing } from "./_components/marketing/Pricing";
-import { Footer } from "./_components/marketing/Footer";
-import { ScrollUpButton } from "./_components/marketing/ScrollUpButton";
+
 
 export default function Home() {
-  return (
-    <>
-      <Hero />
-      <div className="py-20"></div>
-      <Features1 />
-      <CaseStudies />
-      <Divider />
-      <Testimonials />
-      <FeaturesDiagonal />
-      <Pricing />
-      <Brands />
-      <Footer />
-      <ScrollUpButton />
-    </>
-  );
+  return <></>;
 }
