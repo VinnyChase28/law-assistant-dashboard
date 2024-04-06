c10a80ce5aa7eff49c9c2f3d723a34223e8fc7bc
VinnyChase28 <vincentgauthier068@gmail.com>
2024-04-05 23:57:55 -0700

    fixed nav

--- src/app/marketing/components/navbar.tsx
+++ src/app/marketing/components/navbar.tsx
@@ -65,7 +65,7 @@ export const Navbar = () => {
   }
 
   return (
-    <header className="sticky top-0 z-40 w-full  bg-white dark:border-b-slate-700 dark:bg-background">
+    <header className="fixed top-0 z-40 w-full bg-white dark:border-b-slate-700 dark:bg-background">
       <NavigationMenu className="mx-auto">
         <NavigationMenuList className="container flex h-14 w-screen justify-between px-4 ">
           <NavigationMenuItem className="flex items-center font-bold">
