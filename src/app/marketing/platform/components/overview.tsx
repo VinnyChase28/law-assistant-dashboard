c306be54e9a711c2a50d7d063302eacebd163953
VinnyChase28 <vincentgauthier068@gmail.com>
2024-04-05 23:52:53 -0700

    add scrolling animation

--- src/app/marketing/platform/components/overview.tsx
+++ src/app/marketing/platform/components/overview.tsx
@@ -1,5 +1,6 @@
-"use client";
+// Photos from https://citizenofnowhe.re/lines-of-the-city
 
+"use client";
 import { useRef } from "react";
 import {
   motion,
@@ -13,43 +14,32 @@ function useParallax(value: MotionValue<number>, distance: number) {
   return useTransform(value, [0, 1], [-distance, distance]);
 }
 
-interface Feature {
-  id: number;
-  title: string;
-  description: string;
-}
-
-function FeatureSection({ feature }: { feature: Feature }) {
+function Image({ id }: { id: number }) {
   const ref = useRef(null);
   const { scrollYProgress } = useScroll({ target: ref });
   const y = useParallax(scrollYProgress, 300);
 
   return (
-    <section className="relative flex h-screen snap-center items-center justify-center">
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div className="lg:text-center">
-          <motion.h2
-            className="text-base font-semibold uppercase tracking-wide"
-            style={{ y }}
-          >
-            {`Feature ${feature.id}`}
-          </motion.h2>
-          <motion.p
-            className="tracking-tigh mt-2 text-3xl font-extrabold leading-8 sm:text-4xl"
-            style={{ y }}
-          >
-            {feature.title}
-          </motion.p>
-          <motion.p className="mt-4 max-w-2xl text-xl lg:mx-auto" style={{ y }}>
-            {feature.description}
-          </motion.p>
-        </div>
+    <section className="perspective-[500px] relative flex h-screen snap-center items-center justify-center">
+      <div
+        ref={ref}
+        className="relative mx-5 h-[400px] max-h-[90vh] w-[300px] overflow-hidden bg-white"
+      >
+        <img
+          className="absolute bottom-0 left-0 right-0 top-0 h-full w-full"
+          src={`/${id}.jpg`}
+          alt="A London skyscraper"
+        />
       </div>
+      <motion.h2
+        className="absolute left-[calc(50%+130px)] m-0 text-7xl font-bold leading-tight tracking-tighter text-[var(--accent)]"
+        style={{ y }}
+      >{`#00${id}`}</motion.h2>
     </section>
   );
 }
 
-export default function PlatformOverview() {
+export default function App() {
   const { scrollYProgress } = useScroll();
   const scaleX = useSpring(scrollYProgress, {
     stiffness: 100,
@@ -57,35 +47,13 @@ export default function PlatformOverview() {
     restDelta: 0.001,
   });
 
-  const features: Feature[] = [
-    {
-      id: 1,
-      title: "Out-of-the box and custom integrations",
-      description:
-        "Whether it's one of the industry-leading CCaaS platforms or a homegrown CRM, we seamlessly integrate to quickly deploy in weeks instead of months or years.",
-    },
-    {
-      id: 2,
-      title: "Out-of-the box and custom integrations",
-      description:
-        "Whether it's one of the industry-leading CCaaS platforms or a homegrown CRM, we seamlessly integrate to quickly deploy in weeks instead of months or years.",
-    },
-    {
-      id: 3,
-      title: "Out-of-the box and custom integrations",
-      description:
-        "Whether it's one of the industry-leading CCaaS platforms or a homegrown CRM, we seamlessly integrate to quickly deploy in weeks instead of months or years.",
-    },
-    // Add more features here
-  ];
-
   return (
     <>
-      {features.map((feature) => (
-        <FeatureSection key={feature.id} feature={feature} />
+      {[1, 2, 3, 4, 5].map((image) => (
+        <Image key={image} id={image} />
       ))}
       <motion.div
-        className="fixed bottom-24 left-0 right-0 h-1 bg-white"
+        className="fixed bottom-24 left-0 right-0 h-[5px] bg-[var(--accent)]"
         style={{ scaleX }}
       />
     </>
