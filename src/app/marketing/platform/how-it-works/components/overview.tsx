7789599a912abd78ba33afcdaf81f59db8669a6e
VinnyChase28 <vincentgauthier068@gmail.com>
2024-04-07 16:23:55 -0700

    add how it works

---
+++ src/app/marketing/platform/how-it-works/components/overview.tsx
@@ -0,0 +1,9 @@
+import FeatureList from "../../../components/features/feature-list";
+
+export default function Overview() {
+  return (
+    <>
+      <FeatureList slug="how-it-works" />
+    </>
+  );
+}
---
+++ src/app/marketing/platform/how-it-works/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Overview from "./components/overview";
+
+
+export default function HowItWorksPage() {
+  return (
+    <div className="container mx-auto px-4 py-12">
+      <Overview />
+    </div>
+  );
+}
