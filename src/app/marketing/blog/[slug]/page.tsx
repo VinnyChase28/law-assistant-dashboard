dbd2326e63407f36da84b60e70eb5a5bba67511b
VinnyChase28 <vincentgauthier068@gmail.com>
2024-04-09 14:52:00 -0700

    test without markdown and animations

--- src/app/marketing/blog/[slug]/page.tsx
+++ src/app/marketing/blog/[slug]/page.tsx
@@ -20,31 +20,31 @@ export default async function BlogPostPage({ params }: { params: { slug: string 
     ]);
 
     return (
-      <BlogAnimations>
-        <div className="container mx-auto px-4 py-10">
-          <article className="mx-auto my-8 max-w-3xl">
-            {post?.coverImage && (
-              <div className="relative mb-8 aspect-video">
-                <Image
-                  src={post.coverImage}
-                  alt={post?.title || "Blog post cover image"}
-                  fill
-                  style={{ objectFit: "cover" }}
-                  className="rounded"
-                />
-              </div>
-            )}
-            <h1 className="mb-4 text-4xl font-bold">{post?.title}</h1>
-            <div className="mb-8 text-gray-500">
-              Published on{" "}
-              {post?.publishedAt
-                ? new Date(post.publishedAt).toLocaleDateString()
-                : "N/A"}
+      // <BlogAnimations>
+      <div className="container mx-auto px-4 py-10">
+        <article className="mx-auto my-8 max-w-3xl">
+          {post?.coverImage && (
+            <div className="relative mb-8 aspect-video">
+              <Image
+                src={post.coverImage}
+                alt={post?.title || "Blog post cover image"}
+                fill
+                style={{ objectFit: "cover" }}
+                className="rounded"
+              />
             </div>
-            <Markdown markdownText={post?.content ?? ""} />
-          </article>
-        </div>
-      </BlogAnimations>
+          )}
+          <h1 className="mb-4 text-4xl font-bold">{post?.title}</h1>
+          <div className="mb-8 text-gray-500">
+            Published on{" "}
+            {post?.publishedAt
+              ? new Date(post.publishedAt).toLocaleDateString()
+              : "N/A"}
+          </div>
+          {/* <Markdown markdownText={post?.content ?? ""} /> */}
+        </article>
+      </div>
+      // </BlogAnimations>
     );
   } catch (error) {
     console.error(`Error fetching blog post with slug ${params.slug}:`, error);
