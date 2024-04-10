8ccec6c20ff9d8b456acd040b63b9e3f54c01b2b
VinnyChase28 <vincentgauthier068@gmail.com>
2024-04-09 17:34:40 -0700

    update blog

--- src/app/marketing/blog/[slug]/page.tsx
+++ src/app/marketing/blog/[slug]/page.tsx
@@ -1,64 +1,31 @@
-import {
-  getDocumentBySlug,
-  getDocumentPaths,
-  getDocumentSlugs,
-} from "outstatic/server";
-import BlogAnimations from "../components/blog-animations";
-import Markdown from "src/app/_components/markdown";
-import Image from "next/image";
+import { getDocumentSlugs, getDocumentBySlug } from "outstatic/server";
 
-export const dynamic = "force-static";
-
-
+// Return a list of `params` to populate the [slug] dynamic segment
 export async function generateStaticParams() {
-  const slugs = await getDocumentSlugs("blog");
-  return slugs.map((slug) => ({ params: { slug } }));
+  const posts = getDocumentSlugs("blog");
+  console.log(posts);
+  return posts.map((slug) => ({ slug }));
 }
+// Multiple versions of this page will be statically generated
+// using the `params` returned by `generateStaticParams`
+export default async function Page({ params }: { params: { slug: string } }) {
+  const { slug } = params;
+  console.log(slug);
+  const post = await getDocumentBySlug('blog', params.slug, [
+    'title',
+    'author',
+    'content',
+    'coverImage',
+    'publishedAt',
+  ]);
+  console.log(post);
 
-export default async function BlogPostPage({
-  params,
-}: {
-  params: { slug: string };
-}) {
-  try {
-    const post = await getDocumentBySlug("blog", params.slug, [
-      "title",
-      "content",
-      "publishedAt",
-      "coverImage",
-    ]);
+  return (
+    <div>
+      <h1>{post?.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: post?.content || "" }} />
+    </div>
+  );
 
-    console.log(post);
-
-    return (
-      <BlogAnimations>
-        <div className="container mx-auto px-4 py-10">
-          <article className="mx-auto my-8 max-w-3xl">
-            {post?.coverImage && (
-              <div className="relative mb-8 aspect-video">
-                <Image
-                  src={post.coverImage}
-                  alt={post?.title || "Blog post cover image"}
-                  fill
-                  style={{ objectFit: "cover" }}
-                  className="rounded"
-                />
-              </div>
-            )}
-            <h1 className="mb-4 text-4xl font-bold">{post?.title}</h1>
-            <div className="mb-8 text-gray-500">
-              Published on{" "}
-              {post?.publishedAt
-                ? new Date(post.publishedAt).toLocaleDateString()
-                : "N/A"}
-            </div>
-            <Markdown markdownText={post?.content ?? ""} />
-          </article>
-        </div>
-      </BlogAnimations>
-    );
-  } catch (error) {
-    console.error(`Error fetching blog post with slug ${params.slug}:`, error);
-    return <div>Failed to load the blog post.</div>;
-  }
+  
 }
