49bf125b0f775386706483f12bcc1bbdcd876163
VinnyChase28 <vincentgauthier068@gmail.com>
2024-04-09 18:36:19 -0700

    add proper blog

--- src/app/marketing/blog/[slug]/page.tsx
+++ src/app/marketing/blog/[slug]/page.tsx
@@ -2,33 +2,48 @@ import { getDocumentSlugs, getDocumentBySlug } from "outstatic/server";
 import matter from 'gray-matter';
 import path from 'path';
 import { promises as fs } from 'fs';
+import Markdown from "src/app/_components/markdown";
+import Image
+ from "next/image";
+import BlogAnimations from "../components/blog-animations";
 
-// Return a list of `params` to populate the [slug] dynamic segment
 export async function generateStaticParams() {
   const posts = getDocumentSlugs("blog");
   console.log(posts);
   return posts.map((slug) => ({ slug }));
 }
 
-
-
 export default async function Page({ params }: { params: { slug: string } }) {
   const { slug } = params;
-
-  // Construct the full path to the markdown file
   const filePath = path.join(process.cwd(), 'outstatic', 'content', 'blog', `${slug}.md`);
   const fileContents = await fs.readFile(filePath, 'utf8');
-
-  // Parse the markdown file content
   const { data: frontMatter, content } = matter(fileContents);
 
-  // Here, you can convert `content` from markdown to HTML as needed
-
   return (
-    <div>
-      <h1>{frontMatter.title}</h1>
-      {/* Render the markdown content as HTML */}
-      <div dangerouslySetInnerHTML={{ __html: content }} />
-    </div>
+    <BlogAnimations>
+        <div className="container mx-auto px-4 py-10">
+          <article className="mx-auto my-8 max-w-3xl">
+            {frontMatter?.coverImage && (
+              <div className="relative mb-8 aspect-video">
+                <Image
+                  src={frontMatter.coverImage}
+                  alt={frontMatter?.title || "Blog post cover image"}
+                  fill
+                  style={{ objectFit: "cover" }}
+                  className="rounded"
+                />
+              </div>
+            )}
+            <h1 className="mb-4 text-4xl font-bold">{frontMatter?.title}</h1>
+            <div className="mb-8 text-gray-500">
+              Published on{" "}
+              {frontMatter?.publishedAt
+                ? new Date(frontMatter.publishedAt).toLocaleDateString()
+                : "N/A"}
+            </div>
+            <Markdown markdownText={content ?? ""} />
+          </article>
+        </div>
+      </BlogAnimations>
   );
 }
