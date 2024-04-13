41b9a6c92e77ed363d11adcdb12fa7997d1d065e
VinnyChase28 <vincentgauthier068@gmail.com>
2024-04-12 17:56:35 -0700

    default theme dark

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,7 +54,7 @@ export default async function RootLayout({
         <TRPCReactProvider cookies={cookies().toString()}>
           <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="dark"
             enableSystem
             disableTransitionOnChange
           >
