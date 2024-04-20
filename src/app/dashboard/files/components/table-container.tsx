4672bc1d3c7bad9c28aac669ee104158d81b832a
VinnyChase28 <vincentgauthier068@gmail.com>
2024-04-20 11:51:20 -0700

    improved table skeleton

--- src/app/dashboard/files/components/table-container.tsx
+++ src/app/dashboard/files/components/table-container.tsx
@@ -37,8 +37,37 @@ export default function TableContainer({
 
   if (isLoading) {
     return (
-      <div className="items-center">
-        <IconSpinner />
+      <div
+        role="status"
+        className="w-full animate-pulse space-y-4 divide-y divide-gray-200 rounded border border-gray-200 p-10  shadow dark:divide-gray-700 dark:border-gray-700 md:p-6"
+      >
+        <div className="flex items-center justify-between">
+          <div className="w-full">
+            <div className="mb-2.5 h-2.5 w-3/4 rounded-full"></div>
+            <div className="h-2 w-11/12 rounded-full "></div>
+          </div>
+        </div>
+        {/* Duplicate the skeleton rows as needed */}
+        <div className="flex items-center justify-between pt-4">
+          <div className="w-full">
+            <div className="mb-2.5 h-2.5 w-3/4 rounded-full"></div>
+            <div className="rounded-full0 h-2 w-11/12"></div>
+          </div>
+        </div>
+        {/* Add more skeleton rows as needed */}
+        <div className="flex items-center justify-between pt-4">
+          <div className="w-full">
+            <div className="mb-2.5 h-2.5 w-3/4 rounded-full"></div>
+            <div className="h-2 w-11/12 rounded-full"></div>
+          </div>
+        </div>
+        <div className="flex items-center justify-between pt-4">
+          <div className="w-full">
+            <div className="mb-2.5 h-2.5 w-3/4 rounded-full"></div>
+            <div className="h-2 w-11/12 rounded-full"></div>
+          </div>
+        </div>
+        <span className="sr-only">Loading...</span>
       </div>
     );
   }
