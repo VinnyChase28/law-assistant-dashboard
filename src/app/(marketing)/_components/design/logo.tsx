a3296c374f35b63e99e99e0b61bf532014225a88
VinnyChase28 <vincentgauthier068@gmail.com>
2024-05-11 13:52:48 -0700

    change logo

--- src/app/(marketing)/_components/design/logo.tsx
+++ src/app/(marketing)/_components/design/logo.tsx
@@ -3,16 +3,8 @@
 import * as React from "react";
 import type { SVGProps } from "react";
 
-import { useTheme } from "next-themes";
-
-// Import the logo components
-import SvgLaLogo from "./la-logo";
-import SvgLaLogoLight from "./la-logo-light";
-
 const ThemedLogo: React.FC<SVGProps<SVGSVGElement>> = () => {
-  const { theme } = useTheme();
-
-  return <div>{theme === "dark" ? <SvgLaLogoLight /> : <SvgLaLogo />}</div>;
+  return <h4>l.a.</h4>;
 };
 
 export default ThemedLogo;
